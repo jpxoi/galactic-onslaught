fbb77d8f7419ffecf40a781bdedbeae6cc304a3d
Jean Paul Fernandez <jpxoi@icloud.com>
2023-11-13 15:20:59 +0000

    Add Game Constants and Define a class for Game. Basic Logic for getting the main Window.

--- game_solution.py
+++ game_solution.py
@@ -1,3 +1,22 @@
-import tkinter as tk
-from tkinter import Canvas
+from tkinter import Tk, Canvas, PhotoImage, messagebox
 import random
+
+# CONSTANTS
+GAME_TITLE = "Space Invaders Redux"
+GAME_WIDTH = 1440
+GAME_HEIGHT = 900
+GAME_SPEED = 60
+
+class Game:
+    def __init__(self, master):
+        self.master = master
+        self.master.title(GAME_TITLE)
+        self.master.geometry(f"{GAME_WIDTH}x{GAME_HEIGHT}+0+0")
+
+        self.canvas = Canvas(master, bg="black", width=GAME_WIDTH, height=GAME_HEIGHT)
+        self.canvas.pack()
+
+if __name__ == "__main__":
+    root = Tk()
+    game = Game(root)
+    root.mainloop()
